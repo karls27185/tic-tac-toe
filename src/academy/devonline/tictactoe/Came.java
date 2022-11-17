06a5fea9aadfa66b1a9d1a643f0b56ec9568839f
karls27185 <karls27185@gmail.com>
2022-11-17 03:17:39 +0300

    17.12

---
+++ src/academy/devonline/tictactoe/Came.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package academy.devonline.tictactoe;
+
+import java.util.Random;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public class Came {
+
+    public void play() {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+        dataPrinter.printMappingTable();
+        final GameTable gameTable = new GameTable();
+        if (new Random().nextBoolean()) {
+            computerMove.make(gameTable);
+            dataPrinter.printGameTable(gameTable);
+        }
+        while (true) {
+            userMove.make(gameTable);
+            dataPrinter.printGameTable(gameTable);
+            if (winnerVerifier.isUserWin(gameTable)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (drawVerifier.isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            computerMove.make(gameTable);
+            dataPrinter.printGameTable(gameTable);
+            if (winnerVerifier.isComputerWin(gameTable)) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+            if (drawVerifier.isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+
+    }
+}
