fbc90e0d47f2f6cd48824ddac04527ffb0788e7e
karls27185 <karls27185@gmail.com>
2022-11-18 02:21:41 +0300

    17.20

--- src/academy/devonline/tictactoe/component/DataPrinter.java
+++ src/academy/devonline/tictactoe/component/DataPrinter.java
@@ -17,6 +17,7 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.model.Cell;
 import academy.devonline.tictactoe.model.GameTable;
 
 /**
@@ -26,10 +27,39 @@ import academy.devonline.tictactoe.model.GameTable;
 public class DataPrinter {
 
     public void printMappingTable() {
-
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
     public void printGameTable(final GameTable gameTable) {
-
+/*        System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(0, 0)) + " | " +
+                gameTable.getSign(new Cell(0, 1)) + " | " +
+                gameTable.getSign(new Cell(0, 2)) + " | " );
+        System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(1, 0)) + " | " +
+                gameTable.getSign(new Cell(1, 1)) + " | " +
+                gameTable.getSign(new Cell(1, 2)) + " | " );
+        System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(2, 0)) + " | " +
+                gameTable.getSign(new Cell(2, 1)) + " | " +
+                gameTable.getSign(new Cell(2, 2)) + " | " );
+        System.out.println("-------------");*/
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
