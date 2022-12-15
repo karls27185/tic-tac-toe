1a28b7776bca397a69f2e27ad5849fe436ef5e96
karls27185 <karls27185@gmail.com>
2022-12-15 03:51:41 +0300

    21.005

--- BuildJar.java
+++ BuildJar.java
@@ -47,83 +47,85 @@ public class BuildJar {
         }
 
         // javac -sourcepath src/ -d out/classes src/academy/devonline/tictactoe/Launcher.java
-        compileJavaSourceFiles();
+//        compileJavaSourceFiles();
+        runCmd("javac -sourcepath src/ -d out/classes src/academy/devonline/tictactoe/Launcher.java");
 
         // jar cfe out/tic-tac-toe.jar academy.devonline.tictactoe.Launcher -C out/classes .
-        createJarFile();
+//        createJarFile();
+        runCmd("jar cfe out/tic-tac-toe.jar academy.devonline.tictactoe.Launcher -C out/classes .");
 
         // del /q /f out\classes
         // rmdir /q /s out\classes
         deleteCompiledClassFilesAndPackageDirectories();
     }
 
-    private static void compileJavaSourceFiles() throws IOException {
-        // Create javac options
-        final List<String> sourceFiles = new ArrayList<>();
-        sourceFiles.add("-d");
-        sourceFiles.add("out/classes");
-        Files.walkFileTree(Paths.get("src"), new SimpleFileVisitor<>() {
-            @Override
-            public FileVisitResult visitFile(final Path file,
-                                             final BasicFileAttributes attrs) throws IOException {
-                sourceFiles.add(file.toAbsolutePath().toString());
-                return super.visitFile(file, attrs);
-            }
-        });
+//    private static void compileJavaSourceFiles() throws IOException {
+//        // Create javac options
+//        final List<String> sourceFiles = new ArrayList<>();
+//        sourceFiles.add("-d");
+//        sourceFiles.add("out/classes");
+//        Files.walkFileTree(Paths.get("src"), new SimpleFileVisitor<>() {
+//            @Override
+//            public FileVisitResult visitFile(final Path file,
+//                                             final BasicFileAttributes attrs) throws IOException {
+//                sourceFiles.add(file.toAbsolutePath().toString());
+//                return super.visitFile(file, attrs);
+//            }
+//        });
+//
+//        // Invoke javac
+//        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+//        compiler.run(System.in, System.out, System.err, sourceFiles.toArray(new String[0]));
+//    }
 
-        // Invoke javac
-        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        compiler.run(System.in, System.out, System.err, sourceFiles.toArray(new String[0]));
-    }
-
-    private static void createJarFile() throws IOException {
-        // Create manifest
-        final Manifest manifest = new Manifest();
-        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
-        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, "academy.devonline.tictactoe.Launcher");
-
-        // Create jar file out stream
-        try (FileOutputStream stream = new FileOutputStream("out/tic-tac-toe.jar")) {
-            try (JarOutputStream out = new JarOutputStream(stream, manifest)) {
-                // Get absolute paths to class files
-                final List<Path> files = new ArrayList<>();
-                Files.walkFileTree(Paths.get("out/classes"), new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult visitFile(final Path file,
-                                                     final BasicFileAttributes attrs) throws IOException {
-                        files.add(file);
-                        return super.visitFile(file, attrs);
-                    }
-                });
-
-                final String absoluteClassesRootFolder = Paths.get("out/classes").toAbsolutePath() + "/";
-                // Add class files to jar file
-                for (final Path file : files) {
-                    // Add archive entry
-                    final String classFileAbsolutePath = file.toAbsolutePath().toString();
-                    final String classFileRelativePath = classFileAbsolutePath
-                            .substring(absoluteClassesRootFolder.length())
-                            .replace('\\', '/');
-                    final JarEntry jarAdd = new JarEntry(classFileRelativePath);
-                    jarAdd.setTime(Files.getLastModifiedTime(file).toMillis());
-                    out.putNextEntry(jarAdd);
-
-                    // Write file to archive
-                    try (InputStream in = Files.newInputStream(file)) {
-                        final byte[] buffer = new byte[8192];
-                        while (true) {
-                            int nRead = in.read(buffer, 0, buffer.length);
-                            if (nRead <= 0) {
-                                break;
-                            }
-                            out.write(buffer, 0, nRead);
-                        }
-                    }
-                    out.closeEntry();
-                }
-            }
-        }
-    }
+//    private static void createJarFile() throws IOException {
+//        // Create manifest
+//        final Manifest manifest = new Manifest();
+//        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+//        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, "academy.devonline.tictactoe.Launcher");
+//
+//        // Create jar file out stream
+//        try (FileOutputStream stream = new FileOutputStream("out/tic-tac-toe.jar")) {
+//            try (JarOutputStream out = new JarOutputStream(stream, manifest)) {
+//                // Get absolute paths to class files
+//                final List<Path> files = new ArrayList<>();
+//                Files.walkFileTree(Paths.get("out/classes"), new SimpleFileVisitor<>() {
+//                    @Override
+//                    public FileVisitResult visitFile(final Path file,
+//                                                     final BasicFileAttributes attrs) throws IOException {
+//                        files.add(file);
+//                        return super.visitFile(file, attrs);
+//                    }
+//                });
+//
+//                final String absoluteClassesRootFolder = Paths.get("out/classes").toAbsolutePath() + "/";
+//                // Add class files to jar file
+//                for (final Path file : files) {
+//                    // Add archive entry
+//                    final String classFileAbsolutePath = file.toAbsolutePath().toString();
+//                    final String classFileRelativePath = classFileAbsolutePath
+//                            .substring(absoluteClassesRootFolder.length())
+//                            .replace('\\', '/');
+//                    final JarEntry jarAdd = new JarEntry(classFileRelativePath);
+//                    jarAdd.setTime(Files.getLastModifiedTime(file).toMillis());
+//                    out.putNextEntry(jarAdd);
+//
+//                    // Write file to archive
+//                    try (InputStream in = Files.newInputStream(file)) {
+//                        final byte[] buffer = new byte[8192];
+//                        while (true) {
+//                            int nRead = in.read(buffer, 0, buffer.length);
+//                            if (nRead <= 0) {
+//                                break;
+//                            }
+//                            out.write(buffer, 0, nRead);
+//                        }
+//                    }
+//                    out.closeEntry();
+//                }
+//            }
+//        }
+//    }
 
     private static void deleteCompiledClassFilesAndPackageDirectories() throws IOException {
         Files.walkFileTree(Paths.get("out/classes"), new SimpleFileVisitor<>() {
@@ -141,5 +143,16 @@ public class BuildJar {
                 return super.postVisitDirectory(dir, exc);
             }
         });
+    }
+
+    private static void runCmd(final String cmd) {
+        try {
+            final Process process = Runtime.getRuntime().exec(cmd);
+            process.getErrorStream().transferTo(System.err);
+            process.getInputStream().transferTo(System.out);
+            process.waitFor();
+        } catch (final IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
     }
 }
