77f06eba52f564debdf058ca60efa6bbbfcea1a4
karls27185 <karls27185@gmail.com>
2023-02-15 01:16:36 +0300

    27.031

---
+++ src/main/scripts/start-gui-user-user.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# Fix current dir issue for MacOS. Read more: http://hints.macworld.com/article.php?story=20041217111834902
+cd "$(dirname "$0")" || exit
+# ----------------------------------------------------------------------------------------------------------------------
+./start.sh gui user user
