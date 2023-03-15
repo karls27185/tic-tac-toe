e2773c83de9d8ae1bfbfa74ab6056a7b0cad43d7
karls27185 <karls27185@gmail.com>
2023-03-15 03:11:24 +0300

    30.009

--- src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -20,38 +20,37 @@ package academy.devonline.tictactoe.component.console.keypad;
 import academy.devonline.tictactoe.component.console.CellNumberConverter;
 import academy.devonline.tictactoe.model.game.Cell;
 
+import static java.lang.String.format;
+
 /**
  * @author Karl
  * @link https://babayan.keenetic.link/
  */
 public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
 
-//    private final char[][] mapping = {
-////            {'7', '8', '9'},
-////            {'4', '5', '6'},
-////            {'1', '2', '3'}
-//            {'1', '2', '3'},
-//            {'4', '5', '6'},
-//            {'7', '8', '9'}
-//    };
-
     @Override
     public Cell toCell(final char number) {
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                if (mapping[i][j] == number) {
-//                    return new Cell(i, j);
-//                }
-//            }
-//        }
-//        return null;
-        final int val = number - '0' - 1;
-        return new Cell(val / 3, val % 3);
+        if (number >= '1' && number <= '9') {
+            final int val = number - '0' - 1;
+            return new Cell(val / 3, val % 3);
+        } else {
+            throw new IllegalArgumentException(
+                    format("Number parameter must be between '1' and '9'! Current value is '%s'!", number)
+            );
+        }
     }
 
     @Override
     public char toNumber(final Cell cell) {
-//        return mapping[cell.getRow()][cell.getCol()];
-        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        if (cell.getRow() >= 0 && cell.getRow() <= 2 && cell.getCol() >= 0 && cell.getCol() <= 2) {
+            return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        } else {
+            throw new IllegalArgumentException(
+                    format(
+                            "Row and col indexes must be between 0 and 2! Current row is %s, current col is %s!",
+                            cell.getRow(), cell.getCol()
+                    )
+            );
+        }
     }
 }
