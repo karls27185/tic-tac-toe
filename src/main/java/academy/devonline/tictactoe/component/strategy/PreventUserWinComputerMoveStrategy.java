6b17f152fdf42cf3eb3035d69a7da92fd6fb97ac
karls27185 <karls27185@gmail.com>
2023-03-19 02:50:30 +0300

    30.023

---
+++ src/main/java/academy/devonline/tictactoe/component/strategy/PreventUserWinComputerMoveStrategy.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package academy.devonline.tictactoe.component.strategy;
+
+import academy.devonline.tictactoe.component.ComputerMoveStrategy;
+import academy.devonline.tictactoe.model.game.Cell;
+import academy.devonline.tictactoe.model.game.GameTable;
+import academy.devonline.tictactoe.model.game.Sign;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class PreventUserWinComputerMoveStrategy implements ComputerMoveStrategy {
+    @Override
+    public boolean tryToMakeMove(final GameTable gameTable, final Sign sign) {
+        return tryToMakeMoveByRows(gameTable, sign) ||
+                tryToMakeMoveByCols(gameTable, sign) ||
+                tryToMakeMoveByMainDiagonal(gameTable, sign) ||
+                tryToMakeMoveBySecondaryDiagonal(gameTable, sign);
+    }
+
+    private boolean tryToMakeMoveByRows(final GameTable gameTable, final Sign sign) {
+        for (int i = 0; i < 3; i++) {
+            if (tryToMakeMoveUsingLambdaConversion(gameTable, sign, i, (k, j) -> new Cell(k, j))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByCols(final GameTable gameTable, final Sign sign) {
+        for (int i = 0; i < 3; i++) {
+            if (tryToMakeMoveUsingLambdaConversion(gameTable, sign, i, (k, j) -> new Cell(j, k))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToMakeMoveByMainDiagonal(final GameTable gameTable, final Sign sign) {
+        return tryToMakeMoveUsingLambdaConversion(gameTable, sign, -1, (k, j) -> new Cell(j, j));
+    }
+
+    private boolean tryToMakeMoveBySecondaryDiagonal(final GameTable gameTable, final Sign sign) {
+        return tryToMakeMoveUsingLambdaConversion(gameTable, sign, -1, (k, j) -> new Cell(j, 2 - j));
+    }
+
+    private boolean tryToMakeMoveUsingLambdaConversion(final GameTable gameTable,
+                                                       final Sign sign,
+                                                       final int i,
+                                                       final Lambda lambda) {
+        int countEmptyCells = 0;
+        int countSignCells = 0;
+        Cell lastEmptyCell = null;
+        for (int j = 0; j < 3; j++) {
+            final Cell cell = lambda.convert(i, j);
+            if (gameTable.isEmpty(cell)) {
+                lastEmptyCell = cell;
+                countEmptyCells++;
+            } else if (gameTable.getSign(cell) == sign.oppositeSign()) {
+                countSignCells++;
+            } else {
+                break;
+            }
+        }
+        if (countEmptyCells == 1 && countSignCells == 2) {
+            gameTable.setSign(lastEmptyCell, sign);
+            return true;
+        }
+        return false;
+    }
+
+    @FunctionalInterface
+    private interface Lambda {
+        Cell convert(int k, int j);
+    }
+}
--- src/main/java/academy/devonline/tictactoe/model/game/Sign.java
+++ src/main/java/academy/devonline/tictactoe/model/game/Sign.java
@@ -26,6 +26,16 @@ public enum Sign {
     O,
     EMPTY;
 
+    public Sign oppositeSign() {
+        if (this == X) {
+            return O;
+        } else if (this == O) {
+            return X;
+        } else {
+            throw new IllegalStateException("Empty value does not have an opposite one!");
+        }
+    }
+
     @Override
     public String toString() {
         if (this == EMPTY) {
